33e3f7270c7ca012b5d888cb88951514d72929cb
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-26 19:31:51 +0700

    feat: add auth middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,38 @@
+import { verifyAccessToken } from "../utils/jwt.js";
+import { prisma } from "../config/prisma.js";
+import { AppError } from "../utils/AppError.js";
+
+export const authMiddleware = async (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            throw new AppError({
+                message: "Authorization header missing or invalid",
+                statusCode: 401,
+                code: "UNAUTHORIZED"
+            });
+        }
+        const token = authHeader.split(" ")[1];
+        const decoded = verifyAccessToken(token);
+
+        const user = await prisma.user.findUnique({
+            where: { id: decoded.id },
+            include: {
+                wallet: true
+            }
+        });
+
+        if (!user) {
+            throw new AppError({
+                message: "User not found",
+                statusCode: 404,
+                code: "USER_NOT_FOUND"
+            });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
