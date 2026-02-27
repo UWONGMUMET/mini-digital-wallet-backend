b166547213ddaf56742b447bce8349bf20917bdd
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-28 02:33:00 +0700

    feat: add logout controller

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,6 +1,6 @@
 import { AppError } from "../../utils/AppError.js";
 import { success } from "../../utils/response.js";
-import { registerService, loginService } from "./auth.service.js";
+import { registerService, loginService, refreshTokenService, logoutService } from "./auth.service.js";
 
 export const register = async (req, res, next) => {
     try {
@@ -48,9 +48,28 @@ export const refresh = async (req, res, next) => {
                 code: "MISSING_REFRESH_TOKEN"
             });
         }
-        
+
         const token = await refreshTokenService({ refreshToken });
         return success(res, token, "Token refreshed successfully", 200);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const logout = async (req, res, next) => {
+    try {
+        const { refreshToken } = req.body;
+        
+        if (!refreshToken) {
+            throw new AppError({
+                message: "Refresh token is required",
+                statusCode: 400,
+                code: "MISSING_REFRESH_TOKEN"
+            });
+        }
+
+        const token = await logoutService({ refreshToken });
+        return success(res, token, "User logged out successfully", 200);
     } catch (error) {
         next(error);
     }
