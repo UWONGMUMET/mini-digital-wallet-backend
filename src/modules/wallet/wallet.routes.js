7429e757ebe75ebd7f7eec8cb644b1c2f4544380
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-27 17:38:49 +0700

    feat: add withdraw endpoint

--- src/modules/wallet/wallet.routes.js
+++ src/modules/wallet/wallet.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../../middlewares/authMiddleware.js";
-import { deposit, getBalance } from "../../modules/wallet/wallet.controller.js";
+import { deposit, getBalance, withdraw } from "../../modules/wallet/wallet.controller.js";
 
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.use(authMiddleware);
 
 router.get("/balance", getBalance);
 router.post("/deposit", deposit);
+router.post("/withdraw", withdraw);
 
 export default router;
