79191e2b9fa7777f3a920f79ed306c5cec075031
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-27 16:17:36 +0700

    feat: add deposit endpoint

--- src/modules/wallet/wallet.routes.js
+++ src/modules/wallet/wallet.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { authMiddleware } from "../../middlewares/authMiddleware.js";
-import { getBalance } from "../../modules/wallet/wallet.controller.js";
+import { deposit, getBalance } from "../../modules/wallet/wallet.controller.js";
 
 
 const router = express.Router();
 
 router.use(authMiddleware);
+
 router.get("/balance", getBalance);
+router.post("/deposit", deposit);
 
 export default router;
