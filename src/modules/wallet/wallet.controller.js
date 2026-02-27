18fc0f37e9cce396e200ab27b0351406d3f5ecf0
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-27 17:38:30 +0700

    feat: add withdraw controller

--- src/modules/wallet/wallet.controller.js
+++ src/modules/wallet/wallet.controller.js
@@ -1,6 +1,6 @@
 import { success } from "../../utils/response.js";
 import { AppError } from "../../utils/AppError.js";
-import { depositService, getBalanceService } from "../../modules/wallet/wallet.service.js";
+import { depositService, getBalanceService, withdrawService } from "../../modules/wallet/wallet.service.js";
 
 export const getBalance = async (req, res, next) => {
     try {
@@ -46,4 +46,33 @@ export const deposit = async (req, res, next) => {
     catch (error) {
         next(error);
     }
-}
+};
+
+export const withdraw = async (req, res, next) => {
+    try {
+        const { id } = req.user;
+        if (!id) {
+            throw new AppError({
+                message: "User not found",
+                statusCode: 404,
+                code: "USER_NOT_FOUND"
+            });
+        }
+
+        const { amount } = req.body;
+        if (!amount) {
+            throw new AppError({
+                message: "Amount is required",
+                statusCode: 400,
+                code: "AMOUNT_REQUIRED"
+            });
+        }
+
+        const wallet = await withdrawService({ 
+            userId: id, 
+            amount 
+        });
+    } catch (error) {
+        next(error);
+    }
+};
