2ab77c581186537a4e007f3a23c878e86aeaf1a9
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-27 16:16:56 +0700

    feat: add depositService with transaction logic

--- src/modules/wallet/wallet.service.js
+++ src/modules/wallet/wallet.service.js
@@ -17,4 +17,52 @@ export const getBalanceService = async (userId) => {
     }
 
     return wallet;
+};
+
+export const depositService = async ({ userId, amount }) => {
+    if (amount <= 0) {
+        throw new AppError({
+            message: "Invalid amount",
+            statusCode: 400,
+            code: "INVALID_AMOUNT"
+        });
+    }
+
+    return prisma.$transaction(async (tx) => {
+        const wallet = await tx.wallet.findUnique({
+            where: {
+                userId
+            }
+        });
+
+        if (!wallet) {
+            throw new AppError({
+                message: "Wallet not found",
+                statusCode: 404,
+                code: "WALLET_NOT_FOUND"
+            });
+        }
+
+        const updateWallet = await tx.wallet.update({
+            where: {
+                id: wallet.id
+            },
+            data: {
+                balance: {
+                    increment: amount
+                }
+            }
+        });
+
+        await tx.transaction.create({
+            data: {
+                amount,
+                walletId: updateWallet.id,
+                type: "deposit",
+                reference: `DEP-${Date.now()}`
+            }
+        });
+
+        return updateWallet;
+    })
 }
